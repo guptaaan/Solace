0fab6ff39825867655e7be18613a85f94ce5e048
Aanjaneya Gupta <guptaaan@sheridancollege.ca>
2025-10-11 09:55:32 +0530

    added useFrameworkReady hook

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
+  useFrameworkReady();
+
   return (
     <>
       <Stack screenOptions={{ headerShown: false }} />
